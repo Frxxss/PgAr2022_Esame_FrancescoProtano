62ff78131c1332a9d8f143c17950c3ae2df222db
Frxxss <104850232+Frxxss@users.noreply.github.com>
2022-06-11 17:18:24 +0200

    Update ListaArmi.java

--- PgAr2022_Esame_FrancescoProtano/src/it/unibs/fp/adventuretime/ListaArmi.java
+++ PgAr2022_Esame_FrancescoProtano/src/it/unibs/fp/adventuretime/ListaArmi.java
@@ -1,7 +1,7 @@
 package it.unibs.fp.adventuretime;
 
 import it.unibs.fp.mylib.EstrazioniCasuali;
-
+//*La classe ListaArmi elenca e denomina le armi.
 public class ListaArmi {
 	
 	Arma spada = new Arma("Spada degli Abissi", EstrazioniCasuali.estraiIntero(35, 55) );
@@ -10,7 +10,10 @@ public class ListaArmi {
 	Arma lancia = new Arma("Bastone della Giustizia", EstrazioniCasuali.estraiIntero(35, 55));
 	Arma tirapugni = new Arma("ScassaMandibole", EstrazioniCasuali.estraiIntero(35, 55));
 	
-	
+	/**
+	 * il metodo estraiArma sceglie casualmente un arma.
+	 * @return
+	 */
 	public Arma estraiArma() {
 		int sceltaArma = EstrazioniCasuali.estraiIntero(1, 5);
 		Arma arma = new Arma(null,0);
